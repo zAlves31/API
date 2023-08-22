0c50577c1860f2b0ff42d4be3807abea4f93aefe
??? <>
2023-08-22 08:34:19 -0300

    21.08

---
+++ webapi.Filmes/webapi.Filmes/Domains/GeneroDomain.cs
@@ -0,0 +1,12 @@
+﻿namespace webapi.Filmes.Domains
+{
+    /// <summary>
+    /// Classe que representa entidade(tabela) Genero
+    /// </summary>
+    public class GeneroDomain
+    {
+        public int IdGenero { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
---
+++ webapi.Filmes/webapi.Filmes/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,18 @@
+﻿using webapi.Filmes.Domains;
+
+namespace webapi.Filmes.Interfaces
+{
+    /// <summary>
+    /// Interface Responsavel pelo repostitorio GeneroRepository
+    /// Definir os metodos que serao implementados pelo GeneroRepository
+    /// </summary>
+    public interface IGeneroRepository
+    {
+        //tipodeRetorno NomeMetodo(tipoParametro nomeParametro)
+
+        void Cadastrar(GeneroDomain novoGenero);
+        List<GeneroDomain> ListarTodos();
+        void AtualizarIdCorpo(GeneroDomain genero);
+        void Deletar(int id);
+    }
+}
