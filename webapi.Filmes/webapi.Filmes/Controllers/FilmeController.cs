dab4e57e3c78f1c8ecb8fcfc20e663f46727e680
??? <>
2023-08-30 10:53:31 -0300

    30.08

---
+++ webapi.Filmes/webapi.Filmes/Controllers/FilmeController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.Filmes.Domains;
+using webapi.Filmes.Interfaces;
+using webapi.Filmes.Repositories;
+
+namespace webapi.Filmes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmeController : ControllerBase
+    {
+        /// <summary>
+        /// Objeto _filmeRepository que ira receber todos os metodos definidos na interface IFilmeRepository
+        /// </summary>
+        private IFilmeRepository _filmeRepository { get; set; }
+
+
+        /// <summary>
+        /// Instacia objeto _filmeRepository para que haja referencia aos metodos no repositorio
+        /// </summary>
+        public FilmeController()
+        {
+            _filmeRepository = new FilmeRepository();
+        }
+
+
+        /// <summary>
+        /// EndPoint que aciona o metodo ListarTodos do Filme
+        /// </summary>
+        /// <returns>resposta para o usuario(front-end)</returns>>
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            try
+            {
+                //Criar uma lista que recebe os dados da requisicao
+                List<FilmeDomain> listarFilmes = _filmeRepository.ListarTodos();
+
+                //retorna a lista no formato JSON com o status code OK(200)
+                return Ok(listarFilmes);
+            }
+            catch (Exception erro) 
+            {
+                //retorna um status code BadRequest(400) e a mensagem do erro
+                return BadRequest(erro);
+            }
+        }
+
+        /// <summary>
+        /// EndPoint que aciona o metodo de cadastro de filme
+        /// </summary>
+        /// <param name="novoFilme"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Post(FilmeDomain novoFilme)
+        {
+            try
+            {
+                //Fazendo a chamada para o metodo cadastrar passando o objeto como parametro
+                _filmeRepository.Cadastrar(novoFilme);
+
+                return StatusCode(201);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        /// <summary>
+        ///  EndPoint que aciona o metodo de deletar filme por ID
+        /// </summary>
+        /// <param name="id">id do filme a ser deletado</param>
+        /// <returns>status code</returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _filmeRepository.Deletar(id);
+
+                return StatusCode(204);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// EndPoint que aciona o metodo de buscar IdFilme
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                FilmeDomain filmeEncontrado = _filmeRepository.BuscarPorId(id);
+
+                return Ok(filmeEncontrado);
+            }
+            catch(Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+
+
+    }
+}
--- webapi.Filmes/webapi.Filmes/Repositories/FilmeRepository.cs
+++ webapi.Filmes/webapi.Filmes/Repositories/FilmeRepository.cs
@@ -1,10 +1,13 @@
-﻿using webapi.Filmes.Domains;
+﻿using System.Data.SqlClient;
+using webapi.Filmes.Domains;
 using webapi.Filmes.Interfaces;
 
 namespace webapi.Filmes.Repositories
 {
     public class FilmeRepository : IFilmeRepository
     {
+        private string StringConexao = "Data Source = DESKTOP-2B634JF; Initial Catalog = Filmes; User Id = sa; pwd = Senai@134";
+
         public void AtualizarIdCorpo(FilmeDomain filme)
         {
             throw new NotImplementedException();
@@ -17,22 +20,136 @@ namespace webapi.Filmes.Repositories
 
         public FilmeDomain BuscarPorId(int id)
         {
-            throw new NotImplementedException();
+            //Declara a conexao passando a string de conexao como parametro
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                //Declara a query que sera executada
+                string querySelectById = $"SELECT IdFilme, Titulo FROM Filme WHERE IdFilme = @IdFilme";
+
+                //Abre a conexao com o banco de dados
+                con.Open();
+
+                //Declara o Sqldatareader para percorrer a tabela do banco de dados
+                SqlDataReader rdr;
+
+                //Declara o SqlCommand passando a query que sera executada e a conexao com o bd
+                using (SqlCommand cmd = new SqlCommand(querySelectById, con))
+                {
+                    //passa o valor para o parametro @IdFilme
+                    cmd.Parameters.AddWithValue("@IdFilme", id);
+
+                    //Executa a query e armazena os dados no rdr
+                    rdr = cmd.ExecuteReader();
+
+                    //Verifica se o resultado da query retornou algum registro
+                    if (rdr.Read())
+                    {
+                        //Se sim, instancia um novo objeto generoBuscado do tipo FilmeDomain
+                        FilmeDomain filmeBuscado = new FilmeDomain
+                        {
+                            //Atribui a propriedade IdFilme o valor da coluna "IdFilme" da tavela do banco de dados
+                            IdFilme = Convert.ToInt32(rdr["IdFilme"]),
+
+                            //Atribui a propriedade Titulo o valor da coluna "Titulo" da tabela do banco de dados
+                            Titulo = rdr["Titulo"].ToString()
+                        };
+                        //Retorna o generoBuscado com os dados obtidos
+                        return filmeBuscado;
+                    }
+
+                    return null;
+                }
+            }
         }
 
+        /// <summary>
+        /// Cadastrar um novo Filme
+        /// </summary>
+        /// <param name="novoFilme"></param>
         public void Cadastrar(FilmeDomain novoFilme)
         {
-            throw new NotImplementedException();
+            using (SqlConnection con =new SqlConnection(StringConexao))
+            {
+                string queryInsert = "INSERT INTO Filme(Titulo) VALUES ('"+ novoFilme.Titulo +"')";
+
+                using (SqlCommand cmd = new SqlCommand(queryInsert, con)) 
+                {
+                    //passa o valor do parametro filme
+                    cmd.Parameters.AddWithValue("@Titulo", novoFilme.Titulo);
+
+                    //Abre a conexao com o banco de dados
+                    con.Open();
+
+                    //Executar a query (queryInsert)
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
+        /// <summary>
+        /// Deletar um determinado Filme
+        /// </summary>
+        /// <param name="id"></param>
         public void Deletar(int id)
         {
-            throw new NotImplementedException();
+            //Declara a conexao passando a string de conexao como parametro
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                //Declara a query que sera executada
+                string queryDelete = "DELETE FROM Filme WHERE IdFilme = @IdFilme ";
+
+
+                using (SqlCommand cmd = new SqlCommand(queryDelete,con))
+                {
+                    //passa o valor para o parametro IdFilme
+                    cmd.Parameters.AddWithValue("@IdFilme", id);
+
+                    //Abre a conexao com o banco de dados
+                    con.Open();
+
+                    //Executar a query (queryDelete)
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public List<FilmeDomain> ListarTodos()
         {
-            throw new NotImplementedException();
+
+            //Cria uma lista de objetos do tipo Filme
+            List<FilmeDomain>  ListarFilmes =  new List<FilmeDomain>();
+
+            //Declara a SqlConnection passando a string de conexao como parametro
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                //Declara a instrucao a ser executada
+                string querySelectAll = "SELECT Filme.IdFilme, Filme.IdGenero, Filme.Titulo, Genero.Nome FROM Filme INNER JOIN Genero ON Filme.IdGenero = Genero.IdGenero ";
+
+                //Abre a conexao com banco de dados
+                con.Open();
+
+                //Declara o Sqldatareader para percorrer a tabela do banco de dados
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(querySelectAll, con))
+                {
+                    rdr= cmd.ExecuteReader();
+
+                    while(rdr.Read()) 
+                    {
+                        FilmeDomain filme = new FilmeDomain()
+                        {
+                            IdFilme = Convert.ToInt32(rdr[0]),
+
+                            Titulo = rdr["Titulo"].ToString()
+                        };
+
+                        ListarFilmes.Add(filme);
+                    }
+                }
+            }
+
+            return ListarFilmes;
         }
     }
 }
